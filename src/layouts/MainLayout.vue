9a98b9253b83b3eb994c12c671d40693181b6019
unknown <you@example.com>
2020-11-10 00:35:40 +0800

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+<q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+        <q-toolbar>
+            <q-btn flat dense round icon="menu" aria-label="Menu" />
+            <q-menu>
+              <q-list style="min-width">
+                <q-item clickable>
+                  <q-item-section>Account</q-item-section>
+                  <q-item-section side>
+                    <q-icon name="keyboard_arrow_right"></q-icon>
+                  </q-item-section>
+                  <q-menu anchor="top right" self="top left">
+                    <q-list>
+                      <q-item clickable to="/account/relink">
+                        <q-item-section>Re-map Prospect</q-item-section>
+                      </q-item>
+                      <q-item clickable to="/">
+                        <q-item-section>To Prospect</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-item>
+              </q-list>
+            </q-menu>
+            <q-toolbar-title>
+                Jupiter Agent
+            </q-toolbar-title>
+
+            <div>{{loginAs}}</div>
+        </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+        <router-view />
+    </q-page-container>
+</q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      loginAs: 'Seven Chen'
+    }
+  }
+}
+</script>
---
+++ src/pages/Account/Relink.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="q-pa-md">
+      <q-btn label="Reset" push color="white" text-color="primary" @click="reset" class="q-mb-md" />
+      <q-stepper
+        v-model="step"
+        header-nav
+        ref="stepper"
+        color="primary"
+        animated
+      >
+      <q-step
+        name="1"
+        title="Put your account info"
+        icon="settings"
+        :done="done1"
+      >
+        <textarea name="" id="" cols="30" rows="10" v-model="prospects"></textarea>
+
+       <q-stepper-navigation>
+          <q-btn @click="() => { done1 = true; step = 2 }" color="primary" label="Continue" />
+        </q-stepper-navigation>
+      </q-step>
+      <q-step
+        :name="2"
+        title="Confirm your put info"
+        icon="create_new_folder"
+        :done="done2"
+      >
+        info to confirm
+        <div>{{prospects.split(/(?:\n|\n\r)/g)}}</div>
+        <q-stepper-navigation>
+          <q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="Continue" />
+          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+      <q-step
+        :name="3"
+        title="Save new prospect info"
+        icon="add_comment"
+        :done="done3"
+      >
+      Finish
+      <q-stepper-navigation>
+          <q-btn color="primary" @click="done3 = true" label="Finish" />
+          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+      </q-stepper>
+    </div>
+</template>
+<script>
+export default {
+  name: 'AccountRelink',
+  data () {
+    return {
+      title: 'Account Relink Page',
+      prospects: '',
+      step: 1,
+      done1: false,
+      done2: false,
+      done3: false
+    }
+  },
+  methods: {
+    reset () {
+      this.done1 = false
+      this.done2 = false
+      this.done3 = false
+      this.step = 1
+    }
+  }
+}
+</script>
